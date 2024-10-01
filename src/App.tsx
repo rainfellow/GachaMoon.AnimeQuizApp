b189291904f59a53f6c2b7029f62c0d20d35075c
akkkihi <akkihiguy@gmail.com>
2024-10-01 13:53:48 +0300

    set dark theme as default

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,48 @@ import '@mantine/core/styles.css';
 import { Loader, MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
-import { Suspense, useContext, useEffect, useState } from 'react';
+import React, { Suspense, useContext, useEffect, useState } from 'react';
 import { useLocalStorage } from './hooks/use-local-storage';
 import { useAuth } from './hooks/use-auth';
 import { AuthContext } from './context/auth-context';
 
 export default function App() {
-  
-  const [initialized, setInitialized] = useState(false);
 
-  const { getItem } = useLocalStorage();
-  const { login, isAuthenticated } = useAuth();
-  const { accountInfo } = useContext(AuthContext);
-  
-  useEffect(() => {
-    if (!initialized) {
-        if (!isAuthenticated()) {
-            const account = getItem("account");
-            if (account) {
-                login(JSON.parse(account));
+    const [initialized, setInitialized] = useState(false);
+    const defaultColorTheme = "dark"
+    const { getItem } = useLocalStorage();
+    const { login, isAuthenticated } = useAuth();
+    const { accountInfo } = useContext(AuthContext);
+
+    useEffect(() => {
+        const themeLocalKeyName = "mantine-color-scheme-value";
+        const themeLocalStorageValue = getItem(themeLocalKeyName);
+        localStorage.setItem(themeLocalKeyName, themeLocalStorageValue ?? defaultColorTheme);
+    }, []);
+
+    useEffect(() => {
+        if (!initialized) {
+            if (!isAuthenticated()) {
+                const account = getItem("account");
+                if (account) {
+                    login(JSON.parse(account));
+                }
             }
+            setInitialized(true);
         }
-        setInitialized(true);
-    }
-}, []);
-  return (
-    <MantineProvider theme={theme}>
-       <Suspense
-        fallback={
-          <div className="pt-3 text-center">
-            <Loader color="primary" variant="grow" />
-          </div>
-        }
-      >
-      { initialized && <Router />}
-      </Suspense>
-    </MantineProvider>
-  );
+    }, []);
+
+    return (
+        <MantineProvider defaultColorScheme={defaultColorTheme} theme={theme}>
+            <Suspense
+                fallback={
+                  <div className="pt-3 text-center">
+                    <Loader color="primary" variant="grow" />
+                  </div>
+                }
+            >
+          { initialized && <Router />}
+          </Suspense>
+        </MantineProvider>
+    );
 }
