ab4680a74576883c556763251a9cef9d47ab66a2
rainfellow <yariktoruk@gmail.com>
2024-10-03 21:15:20 +0300

    removed obsolete code

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,13 +3,10 @@ import { HomePage } from './pages/Home.page';
 import { LoginPage } from './pages/Login/Login.page';
 import { PrivateRoute } from './components/common/private-route';
 import { DiscordAuth } from './components/discordAuth';
-import { SoloGameContextProvider } from './context/solo-game-context';
-import { SoloLobbyView } from './views/SoloLobby/SoloLobbyView';
-import { AnimeContextProvider } from './context/anime-context';
 import { AccountSettingsView } from './views/AccountSettings/AccountSettingsView';
 import SoloLobby from './components/SoloLobby/SoloLobby';
 
-const router2 = createBrowserRouter(createRoutesFromElements(
+const router = createBrowserRouter(createRoutesFromElements(
   <>
   <Route path="/" element={<PrivateRoute component={<HomePage/>}/>}>
     <Route path="sololobby" element={<SoloLobby />} />
@@ -22,21 +19,6 @@ const router2 = createBrowserRouter(createRoutesFromElements(
   </>
 ))
 
-const router = createBrowserRouter([
-  {
-    path: '*',
-    element: <PrivateRoute component={<HomePage/>}/>,
-  },
-  {
-    path: '/auth/sign-in',
-    element: <LoginPage />,
-  },
-  {
-    path: '/auth/discord',
-    element: <DiscordAuth />,
-  },
-]);
-
 export function Router() {
-  return <RouterProvider router={router2} />;
+  return <RouterProvider router={router} />;
 }
